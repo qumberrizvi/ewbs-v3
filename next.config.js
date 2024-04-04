7ec318c5f20f12e0db21d456d55c666dcded7624
Qumber Rizvi <qumberrizvi@ymail.com>
2024-04-04 07:51:49 +0530

    Redirection config

--- next.config.js
+++ next.config.js
@@ -6,7 +6,113 @@ const nextConfig = {
                 source: '/rakez-freezone',
                 destination: '/freezones/rakez',
                 permanent: true,
-            }
+            },
+            {
+                source: '/Dubai-Freezone',
+                destination: '/freezones/dubai',
+                permanent: true,
+            },
+            {
+                source: '/shams-freezone',
+                destination: '/freezones/shams',
+                permanent: true,
+            },
+            {
+                source: '/ajman-media-city-freezone',
+                destination: '/freezones/amc',
+                permanent: true,
+            },
+            {
+                source: '/ifza-freezone',
+                destination: '/freezones/ifza',
+                permanent: true,
+            },
+            {
+                source: '/pro-services',
+                destination: '/services',
+                permanent: true,
+            },
+            {
+                source: '/mea-and-mofa-attestation',
+                destination: '/services',
+                permanent: true,
+            },
+            {
+                source: '/tax-residency-dubai',
+                destination: '/services',
+                permanent: true,
+            },
+            //     TODO: Add these pages, or make redirects permanent + move them above this comment:
+            {
+                source: '/dubai-mainland-company',
+                destination: '/',
+                permanent: false,
+            },
+            {
+                source: '/dubai-mainland-license',
+                destination: '/',
+                permanent: false,
+            },
+            {
+                source: '/required-documents',
+                destination: '/',
+                permanent: false,
+            },
+            {
+                source: '/professional-license',
+                destination: '/',
+                permanent: false,
+            },
+            {
+                source: '/commercial-license',
+                destination: '/',
+                permanent: false,
+            },
+            {
+                source: '/residency-visa',
+                destination: '/',
+                permanent: false,
+            },
+            {
+                source: '/shams-packages',
+                destination: '/freezones/shams',
+                permanent: false,
+            },
+            {
+                source: '/sharjah-freezone',
+                destination: '/freezones/shams',
+                permanent: false,
+            },
+            {
+                source: '/spcfz',
+                destination: '/',
+                permanent: false,
+            },
+            {
+                source: '/dubai-mainland-license',
+                destination: '/',
+                permanent: false,
+            },
+            {
+                source: '/freelancing',
+                destination: '/',
+                permanent: false,
+            },
+            {
+                source: '/company-setup',
+                destination: '/',
+                permanent: false,
+            },
+            {
+                source: '/Terms-And-Condition',
+                destination: '/',
+                permanent: false,
+            },
+            {
+                source: '/Privacy-Policy',
+                destination: '/',
+                permanent: false,
+            },
         ];
     }
 }
