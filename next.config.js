01aeb754071adc718340278139d557c8dfc6a6ab
Qumber Rizvi <qumberrizvi@ymail.com>
2024-04-04 07:33:30 +0530

    Test: Redirection config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/rakez-freezone',
+                destination: '/freezone/rakez',
+                permanent: true,
+            }
+        ];
+    }
+}
 
 module.exports = nextConfig
